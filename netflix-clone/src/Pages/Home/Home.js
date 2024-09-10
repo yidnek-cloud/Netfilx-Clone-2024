92da02fbdf9bc63f7910da8ff77a67519ea47d38
yidnek-cloud <mcyidne@gmail.com>
2024-09-09 20:18:02 -0400

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../Compoonents/Header/Header'
 import Footer from '../../Compoonents/Footer/Footer'
+import Banner from '../../Compoonents/Banner/Banner'
 
 const Home = () => {
   return (
     <>
       <Header />
+        <Banner />
+       
         
         <Footer />
     </>
